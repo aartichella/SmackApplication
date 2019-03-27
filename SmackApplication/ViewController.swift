5ec9ca1e9b58cc0cfaf454dd0daf3de025a0e35d
Aarti Chella <aartichella@gmail.com>
2019-03-27 14:00:50 -0400

    Initial commit

---
+++ SmackApplication/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SmackApplication
+//
+//  Created by Aarti Chella on 3/27/19.
+//  Copyright © 2019 aarti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
